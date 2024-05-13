539938ff43eeff814e696c0502aeeeb9065585cd
Ruan-Pablo <ruestacio@gmail.com>
2024-05-13 15:27:33 -0300

    tabuleiro pra cima, busca com tabuleiro

--- trabalho_1.py
+++ trabalho_1.py
@@ -1,11 +1,53 @@
 import random
+from collections import deque
+
+
+class Tabuleiro:
+    def __init__(self, tamanho_matriz) -> list:    
+        numeros = list(range(1, tamanho_matriz**2)) # Gera uma lista com os números de 1 a n^2 - 1
+        numeros.append(0) # Adiciona o espaço vazio representado por 0
+        random.shuffle(numeros) # Embaralha os números
+        tabuleiro = []
+        for i in range(0, tamanho_matriz**2, tamanho_matriz): # Divide a lista em sublistas para representar as linhas do tabuleiro
+            linha = numeros[i:i+tamanho_matriz]
+            tabuleiro.append(linha)
+        return tabuleiro  # Implemente a classe Tabuleiro conforme necessário
+    
+    def ehObjetivo(self, tabuleiro:list):
+        tamanho = len(tabuleiro)
+        matriz_meta = []
+        for linha in range(tamanho):
+            linha_meta = []
+            for coluna in range(tamanho):
+                valor = linha * tamanho + coluna + 1
+                if valor == tamanho * tamanho:
+                    valor = 0
+                linha_meta.append(valor)
+            matriz_meta.append(linha_meta)
+
+        return matriz_meta
+    
+    def criarEstadoMeta(self, tamanho):
+        estado_meta = []
+
+        for linha in range(tamanho):
+            linha_meta = []
+            for coluna in range(tamanho):
+                valor = linha * tamanho + coluna + 1
+                if valor == tamanho * tamanho:
+                    valor = 0
+                linha_meta.append(valor)
+            estado_meta.append(linha_meta)
+
+        return estado_meta
+
 
 class No:
     def __init__(self):
         self.tabuleiro = None
         self.pai = None
 
-    def setTabuleiro(self, tabuleiro):
+    def setTabuleiro(self, tabuleiro: Tabuleiro):
         self.tabuleiro = tabuleiro
 
     def getTabuleiro(self):
@@ -28,7 +70,7 @@ class No:
         if linha > 0:
             vNo = No()
             vNo.setPai(self)
-            vNo.setTabuleiro([row[:] for row in self.tabuleiro.m])
+            vNo.setTabuleiro([row[:] for row in self.tabuleiro])
             aux = vNo.getTabuleiro()[linha - 1][coluna]
             vNo.getTabuleiro()[linha - 1][coluna] = 0
             vNo.getTabuleiro()[linha][coluna] = aux
@@ -38,7 +80,7 @@ class No:
         if linha < 2:
             vNo = No()
             vNo.setPai(self)
-            vNo.setTabuleiro([row[:] for row in self.tabuleiro.m])
+            vNo.setTabuleiro([row[:] for row in self.tabuleiro])
             aux = vNo.getTabuleiro()[linha + 1][coluna]
             vNo.getTabuleiro()[linha + 1][coluna] = 0
             vNo.getTabuleiro()[linha][coluna] = aux
@@ -48,7 +90,7 @@ class No:
         if coluna > 0:
             vNo = No()
             vNo.setPai(self)
-            vNo.setTabuleiro([row[:] for row in self.tabuleiro.m])
+            vNo.setTabuleiro([row[:] for row in self.tabuleiro])
             aux = vNo.getTabuleiro()[linha][coluna - 1]
             vNo.getTabuleiro()[linha][coluna - 1] = 0
             vNo.getTabuleiro()[linha][coluna] = aux
@@ -58,12 +100,11 @@ class No:
         if coluna < 2:
             vNo = No()
             vNo.setPai(self)
-            vNo.setTabuleiro([row[:] for row in self.tabuleiro.m])
+            vNo.setTabuleiro([row[:] for row in self.tabuleiro])
             aux = vNo.getTabuleiro()[linha][coluna + 1]
             vNo.getTabuleiro()[linha][coluna + 1] = 0
             vNo.getTabuleiro()[linha][coluna] = aux
             movimentos.append(vNo)
-
         return movimentos
 
     def getPosicaoPecaVazia(self):
@@ -72,54 +113,44 @@ class No:
                 if self.tabuleiro.m[linha][coluna] == 0:
                     return linha, coluna
 
-    def isSolucao(self, tabuleiro:list):
-        tamanho = len(tabuleiro)
-        matriz_meta = []
+    # def representacaoDoTabuleiro(self):
+    #     pass  # Implemente conforme necessário
 
-        for linha in range(tamanho):
-            linha_meta = []
-            for coluna in range(tamanho):
-                valor = linha * tamanho + coluna + 1
-                if valor == tamanho * tamanho:
-                    valor = 0
-                linha_meta.append(valor)
-            matriz_meta.append(linha_meta)
+    # def exibirMovimentos(self):
+    #     pass  # Implemente conforme necessário
 
-        return matriz_meta
-    def criarEstadoMeta(self, tamanho):
-        estado_meta = []
-
-        for linha in range(tamanho):
-            linha_meta = []
-            for coluna in range(tamanho):
-                valor = linha * tamanho + coluna + 1
-                if valor == tamanho * tamanho:
-                    valor = 0
-                linha_meta.append(valor)
-            estado_meta.append(linha_meta)
-
-        return estado_meta
-
-    def representacaoDoTabuleiro(self):
-        pass  # Implemente conforme necessário
-
-    def exibirMovimentos(self):
-        pass  # Implemente conforme necessário
-
-    def caminhoDasPecas(self, no):
-        pass  # Implemente conforme necessário
-
-class Tabuleiro:
-    def __init__(self, tamanho_matriz) -> list:    
-        
-        numeros = list(range(1, tamanho_matriz**2)) # Gera uma lista com os números de 1 a n^2 - 1
-        numeros.append(0) # Adiciona o espaço vazio representado por 0
-        random.shuffle(numeros) # Embaralha os números
-        tabuleiro = []
-        for i in range(0, tamanho_matriz**2, tamanho_matriz): # Divide a lista em sublistas para representar as linhas do tabuleiro
-            linha = numeros[i:i+tamanho_matriz]
-            tabuleiro.append(linha)
-        return tabuleiro  # Implemente a classe Tabuleiro conforme necessário
+    # def caminhoDasPecas(self, no):
+    #     pass  # Implemente conforme necessário
 
 class BuscaEmLargura:
-    pass
+    def busca_em_largura(estado_inicial):
+        fila = deque()
+        visitados = set()
+
+        # Adiciona o estado inicial à fila
+        fila.append(estado_inicial)
+
+        while fila:
+            # Remove o próximo estado da fila
+            estado_atual = fila.popleft()
+
+            # Verifica se o estado atual é o objetivo
+            if Tabuleiro.ehObjetivo(estado_atual):
+                return estado_atual
+
+            # Adiciona o estado atual aos visitados
+            visitados.add(estado_atual)
+
+            # Obtém os movimentos possíveis a partir do estado atual
+            movimentos = estado_atual.movimentosPossiveis()
+
+            # Para cada movimento possível, verifica se já foi visitado e, caso contrário, adiciona à fila
+            for movimento in movimentos:
+                if movimento not in visitados and movimento not in fila:
+                    fila.append(movimento)
+
+        return None  # Nenhum estado objetivo encontrado
+
+    # Exemplo de uso
+    # estado_inicial = ...
+    # estado_objetivo = busca_em_largura(estado_inicial)
