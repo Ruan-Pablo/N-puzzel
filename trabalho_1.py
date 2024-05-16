9f587edec2c647d4c8dfc043fb2ccfe8d7da449f
Ruan-Pablo <ruestacio@gmail.com>
2024-05-16 20:08:29 -0300

    tentando a busca

--- trabalho_1.py
+++ trabalho_1.py
@@ -1,9 +1,11 @@
 import random
 from collections import deque
+from queue import Queue
+
 
 def getPosicaoPecaVazia(tabuleiro):
     for linha in range(len(tabuleiro)):
-        for coluna in range(3):
+        for coluna in range(len(tabuleiro)):
             if tabuleiro[linha][coluna] == 0:
                 return linha, coluna
 
@@ -19,6 +21,10 @@ def ehObjetivo(tabuleiro):
             linha_meta.append(valor)
         estado_meta.append(linha_meta)
     return estado_meta == tabuleiro
+
+def printarTabuleiro(tabuleiro):
+    for i in tabuleiro:
+        print(i)
 
 class Tabuleiro:
     def __init__(self) -> None:
@@ -64,12 +70,12 @@ class Tabuleiro:
             self.tabuleiro[pos_zero[0]][pos_zero[1] - 1] = 0
 
 
-    def criar_tabuleiro(self, tamanho_matriz: int):    
+    def criar_tabuleiro(self, tamanho_matriz: int):
         self.estado_meta = self.criarEstadoMeta(tamanho_matriz)
         self.tabuleiro = self.estado_meta
-        for i in range(100):
+        for i in range(1000*tamanho_matriz):
             ale = random.randint(0, 4)
-            pos_zero = getPosicaoPecaVazia()
+            pos_zero = getPosicaoPecaVazia(self.tabuleiro)
             if ale == 1 and pos_zero[0] > 0:
                 self.subir()
             if ale == 2 and (pos_zero[0] < (tamanho_matriz - 1)):
@@ -108,7 +114,10 @@ class No:
         return self.tabuleiro
     
     def clonarTabuleiro(self):
-        return self.tabuleiro[:]
+        nova_matriz = []
+        for row in self.tabuleiro:
+            nova_matriz.append(row[:])
+        return nova_matriz
 
     def setPai(self, pai):
         self.pai = pai
@@ -118,23 +127,24 @@ class No:
 
     def movimentosPossiveis(self):
         movimentos = []
-        linha, coluna = self.getPosicaoPecaVazia()
+        linha, coluna = getPosicaoPecaVazia(self.tabuleiro)
+        tamanho_tabuleiro = len(self.tabuleiro)
 
         # Movimento para cima
         if linha > 0:
             vNo = No()
             vNo.setPai(self)
-            vNo.setTabuleiro([row[:] for row in self.tabuleiro])
+            vNo.setTabuleiro(self.clonarTabuleiro())
             aux = vNo.getTabuleiro()[linha - 1][coluna]
             vNo.getTabuleiro()[linha - 1][coluna] = 0
             vNo.getTabuleiro()[linha][coluna] = aux
             movimentos.append(vNo)
 
         # Movimento para baixo
-        if linha < 2:
+        if linha < (tamanho_tabuleiro - 1):
             vNo = No()
             vNo.setPai(self)
-            vNo.setTabuleiro([row[:] for row in self.tabuleiro])
+            vNo.setTabuleiro(self.clonarTabuleiro())
             aux = vNo.getTabuleiro()[linha + 1][coluna]
             vNo.getTabuleiro()[linha + 1][coluna] = 0
             vNo.getTabuleiro()[linha][coluna] = aux
@@ -144,21 +154,22 @@ class No:
         if coluna > 0:
             vNo = No()
             vNo.setPai(self)
-            vNo.setTabuleiro([row[:] for row in self.tabuleiro])
+            vNo.setTabuleiro(self.clonarTabuleiro())
             aux = vNo.getTabuleiro()[linha][coluna - 1]
             vNo.getTabuleiro()[linha][coluna - 1] = 0
             vNo.getTabuleiro()[linha][coluna] = aux
             movimentos.append(vNo)
 
         # Movimento para direita
-        if coluna < 2:
+        if coluna < (tamanho_tabuleiro - 1):
             vNo = No()
             vNo.setPai(self)
-            vNo.setTabuleiro([row[:] for row in self.tabuleiro])
+            vNo.setTabuleiro(self.clonarTabuleiro())
             aux = vNo.getTabuleiro()[linha][coluna + 1]
             vNo.getTabuleiro()[linha][coluna + 1] = 0
             vNo.getTabuleiro()[linha][coluna] = aux
             movimentos.append(vNo)
+        # print(movimentos)
         return movimentos
 
     def getPosicaoPecaVazia(self):
@@ -167,42 +178,53 @@ class No:
                 if self.tabuleiro[linha][coluna] == 0:
                     return linha, coluna
 
-    # def representacaoDoTabuleiro(self):
-    #     pass  # Implemente conforme necessário
+    def exibirMovimentos(self, movimentos:list):
+        for obj in movimentos:
+            print(obj.tabuleiro)
+    
+    def passos(self):
+        current = self
+        passos = [current]
+        
+        while current.pai is not None:
+            current = current.pai
+            passos.append(current)
+        
+        passos.reverse()
+        
+        return passos
 
-    # def exibirMovimentos(self):
-    #     pass  # Implemente conforme necessário
 
     # def caminhoDasPecas(self, no):
     #     pass  # Implemente conforme necessário
 
 class BuscaEmLargura:
     def busca_em_largura(estado_inicial):
-        fila = deque()
+        fila: Queue[No] = deque()
         visitados = set()
 
         # Adiciona o estado inicial à fila
         fila.append(estado_inicial)
 
         while fila:
-            # Remove o próximo estado da fila
             estado_atual = fila.popleft()
 
-            # Verifica se o estado atual é o objetivo
             if ehObjetivo(estado_atual.getTabuleiro()):
                 return estado_atual
 
-            # Adiciona o estado atual aos visitados
             visitados.add(estado_atual)
-
-            # Obtém os movimentos possíveis a partir do estado atual
             movimentos = estado_atual.movimentosPossiveis()
 
             # Para cada movimento possível, verifica se já foi visitado e, caso contrário, adiciona à fila
-            for movimento in movimentos:
+            for index, movimento in enumerate(movimentos):
                 if movimento not in visitados and movimento not in fila:
+                    # movimento.printarTabuleiro(movimento.tabuleiro)
+                    print('----------------------')
+                    printarTabuleiro(movimento.tabuleiro)
                     fila.append(movimento)
-
+                    # print('foi visitado')
+        print("fim da busca")
+        estado_atual.passos()
         return None  # Nenhum estado objetivo encontrado
 
 # Exemplo de uso
@@ -211,10 +233,9 @@ class BuscaEmLargura:
 
 tabuleiro = Tabuleiro()
 tabuleiro.criar_tabuleiro(3)
-# print(tabuleiro.getTabuleiro())
 
 puzzel = No()
 puzzel.setTabuleiro(tabuleiro.getTabuleiro())
-
-
-print(BuscaEmLargura.busca_em_largura(puzzel))
+puzzel.setPai(puzzel)
+final = BuscaEmLargura.busca_em_largura(puzzel)
+print(final.tabuleiro)
