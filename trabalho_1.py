bcffca9304cb573ebd3e73e35af489a6591e8a16
Ruan-Pablo <ruestacio@gmail.com>
2024-05-16 21:31:11 -0300

    bfs +/-ok, achar maneira de criar a tabela de forma solucionavel

--- trabalho_1.py
+++ trabalho_1.py
@@ -40,50 +40,84 @@ class Tabuleiro:
         for i in self.tabuleiro:
             print(i)
     
+    def clonarTabuleiro(self):
+        nova_matriz = []
+        for row in self.tabuleiro:
+            nova_matriz.append(row[:])
+        return nova_matriz
+
     def subir(self):
         pos_zero = getPosicaoPecaVazia(self.tabuleiro)
+        tabuleiro = self.clonarTabuleiro()
         if pos_zero[0] > 0:
-            aux = self.tabuleiro[pos_zero[0] - 1][pos_zero[1]] 
-            self.tabuleiro[pos_zero[0]][pos_zero[1]] = aux
-            self.tabuleiro[pos_zero[0] - 1][pos_zero[1]] = 0
+            aux = tabuleiro[pos_zero[0] - 1][pos_zero[1]] 
+            tabuleiro[pos_zero[0]][pos_zero[1]] = aux
+            tabuleiro[pos_zero[0] - 1][pos_zero[1]] = 0
+            self.tabuleiro = tabuleiro
+
+            return tabuleiro
         
     def descer(self):
         pos_zero = getPosicaoPecaVazia(self.tabuleiro)
+        tabuleiro = self.clonarTabuleiro()
         if pos_zero[0] < (len(self.tabuleiro) - 1):
-            aux = self.tabuleiro[pos_zero[0] + 1][pos_zero[1]] 
-            self.tabuleiro[pos_zero[0]][pos_zero[1]] = aux
-            self.tabuleiro[pos_zero[0] + 1][pos_zero[1]] = 0
+            aux = tabuleiro[pos_zero[0] + 1][pos_zero[1]] 
+            tabuleiro[pos_zero[0]][pos_zero[1]] = aux
+            tabuleiro[pos_zero[0] + 1][pos_zero[1]] = 0
+            self.tabuleiro = tabuleiro
+
+            return tabuleiro
             
     def direitar(self):
         pos_zero = getPosicaoPecaVazia(self.tabuleiro)
+        tabuleiro = self.clonarTabuleiro()
         if pos_zero[1] < (len(self.tabuleiro) - 1):
-            aux = self.tabuleiro[pos_zero[0]][pos_zero[1] + 1] 
-            self.tabuleiro[pos_zero[0]][pos_zero[1]] = aux
-            self.tabuleiro[pos_zero[0]][pos_zero[1] + 1] = 0
+            aux = tabuleiro[pos_zero[0]][pos_zero[1] + 1] 
+            tabuleiro[pos_zero[0]][pos_zero[1]] = aux
+            tabuleiro[pos_zero[0]][pos_zero[1] + 1] = 0
+            self.tabuleiro = tabuleiro
+
+            return tabuleiro
             # print(getPosicaoPecaVazia(self.tabuleiro))
 
     def esquerdar(self):
         pos_zero = getPosicaoPecaVazia(self.tabuleiro)
+        tabuleiro = self.clonarTabuleiro()
         if pos_zero[1] > 0:
-            aux = self.tabuleiro[pos_zero[0]][pos_zero[1] - 1] 
-            self.tabuleiro[pos_zero[0]][pos_zero[1]] = aux
-            self.tabuleiro[pos_zero[0]][pos_zero[1] - 1] = 0
+            aux = tabuleiro[pos_zero[0]][pos_zero[1] - 1] 
+            tabuleiro[pos_zero[0]][pos_zero[1]] = aux
+            tabuleiro[pos_zero[0]][pos_zero[1] - 1] = 0
+            self.tabuleiro = tabuleiro
 
+            return tabuleiro
+
+    def movimentoAleatorio(self):
+        tabuleiro = []
+        pos_zero = getPosicaoPecaVazia(self.tabuleiro)
+        if pos_zero[0] > 0:
+            tabuleiro.append(self.subir())
+            # print('subi')
+        if pos_zero[0] < (len(self.tabuleiro) - 1):
+            tabuleiro.append(self.descer())
+            # print('desci')
+
+        if pos_zero[1] > 0:
+            tabuleiro.append(self.esquerdar())
+            # print('esq')
+
+        if pos_zero[1] < (len(self.tabuleiro) - 1):
+            tabuleiro.append(self.direitar())
+            # print('dir')
+
+        return tabuleiro
 
     def criar_tabuleiro(self, tamanho_matriz: int):
         self.estado_meta = self.criarEstadoMeta(tamanho_matriz)
         self.tabuleiro = self.estado_meta
-        for i in range(1000*tamanho_matriz):
-            ale = random.randint(0, 4)
-            pos_zero = getPosicaoPecaVazia(self.tabuleiro)
-            if ale == 1 and pos_zero[0] > 0:
-                self.subir()
-            if ale == 2 and (pos_zero[0] < (tamanho_matriz - 1)):
-                self.descer()
-            if ale == 3 and (pos_zero[1] < (tamanho_matriz - 1)):
-                self.direitar()
-            if ale == 4 and pos_zero[1] > 0:
-                self.esquerdar()
+
+        for _ in range(1000):
+            self.tabuleiro = random.choice(self.movimentoAleatorio())
+
         self.printarTabuleiro()
     
     def ehObjetivo(self, tabuleiro):
@@ -129,7 +163,6 @@ class No:
         movimentos = []
         linha, coluna = getPosicaoPecaVazia(self.tabuleiro)
         tamanho_tabuleiro = len(self.tabuleiro)
-
         # Movimento para cima
         if linha > 0:
             vNo = No()
@@ -174,7 +207,7 @@ class No:
 
     def getPosicaoPecaVazia(self):
         for linha in range(len(self.tabuleiro)):
-            for coluna in range(3):
+            for coluna in range(len(self.tabuleiro)):
                 if self.tabuleiro[linha][coluna] == 0:
                     return linha, coluna
 
@@ -187,11 +220,11 @@ class No:
         passos = [current]
         
         while current.pai is not None:
+            # print(current.pai.tabuleiro)
             current = current.pai
             passos.append(current)
         
         passos.reverse()
-        
         return passos
 
 
@@ -205,26 +238,24 @@ class BuscaEmLargura:
 
         # Adiciona o estado inicial à fila
         fila.append(estado_inicial)
-
         while fila:
             estado_atual = fila.popleft()
 
             if ehObjetivo(estado_atual.getTabuleiro()):
-                return estado_atual
+                print('fim da busca')
+                
+                return estado_atual.passos()
 
             visitados.add(estado_atual)
             movimentos = estado_atual.movimentosPossiveis()
 
             # Para cada movimento possível, verifica se já foi visitado e, caso contrário, adiciona à fila
-            for index, movimento in enumerate(movimentos):
-                if movimento not in visitados and movimento not in fila:
-                    # movimento.printarTabuleiro(movimento.tabuleiro)
-                    print('----------------------')
-                    printarTabuleiro(movimento.tabuleiro)
+            for movimento in movimentos:
+                if movimento not in visitados:
+                    # print(movimento.pai.tabuleiro)
                     fila.append(movimento)
-                    # print('foi visitado')
-        print("fim da busca")
-        estado_atual.passos()
+
+        
         return None  # Nenhum estado objetivo encontrado
 
 # Exemplo de uso
@@ -236,6 +267,8 @@ tabuleiro.criar_tabuleiro(3)
 
 puzzel = No()
 puzzel.setTabuleiro(tabuleiro.getTabuleiro())
-puzzel.setPai(puzzel)
+
 final = BuscaEmLargura.busca_em_largura(puzzel)
-print(final.tabuleiro)
+for i, state in enumerate(final):
+        print('-' * 10 + f' Passo {i} ' + '-' * 10)
+        printarTabuleiro(state.tabuleiro)
